764a4f0c146da0c38a9f3780177f822995a91e02
hiroyukitanaka1126 <h-tanaka26@outlook.jp>
2018-12-06 17:33:28 +0900

    Edit.message.js（add.update_message)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,12 @@
 $(function(){
 
+  // 送信したメッセージ（HTML）の作成
   function buildHTML(message){
     var image = " ";
     if(message.image){
       image = `<image src = "${message.image}">`;
     };
-    var html = `<div class="messages">
+    var html = `<div class="messages" data-massage-id="${message.id}">
                  <div class="messages__name">
                   ${message.user_name}
                  </div>
@@ -22,6 +23,7 @@ $(function(){
     return html;
   };
 
+  // メッセージ送信を非同期通信で行う
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -40,9 +42,41 @@ $(function(){
       $('.chat').append(html);
       $('.chat').animate({scrollTop: $('.chat')[0].scrollHeight},'fast');
       $('.submit-form').prop('disabled', false);
+      $('.type-message').val('')
+      $('#message_image').val('')
     })
-     .fail(function(){
-      alert('error');
+     .fail(function(data){
+      alert('メッセージを入力してください');
     });
+     return false;
   });
+
+  // メッセージの自動更新
+  var interval = setInterval(function() {
+    if(window.location.href.match(/\/groups\/\d+\/messages/)){
+      var id = $('.messages:last').data('messageId');
+
+      $.ajax({
+        url: location.href,
+        data: { id : id },
+        type: "GET",
+        dataType: 'json'
+      })
+      .done(function(data){
+        var HTML = '';
+        data.messages.forEach(function(message){
+          if(message.id > id){
+            HTML += buildHTML(message);
+            $('.chat').append(HTML);
+            $('.chat').animate({scrollTop: $('.chat')[0].scrollHeight},'fast');
+          }
+        })
+      })
+      .fail(function(){
+        alert('error');
+      });
+    }
+    else {
+      clearInterval(interval);
+    }} , 5000 );
 });
