4d3e7df0b3f4570587ef998ded5369104f8c6cd2
hiroyukitanaka1126 <h-tanaka26@outlook.jp>
2018-12-12 20:19:20 +0900

    edit.user.js(add.tubolinks)

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,4 @@
-  $(function(){
+$(document).on('turbolinks:load', function() {
     function appendUserName(user){
       var html = `<div class="chat-group-user clearfix">
                     <p class="chat-group-user__name">${user.name}</p>
